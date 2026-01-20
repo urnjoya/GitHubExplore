f1a83fa8f9a8e769a720b36822316981bbf74870
Joya <urnjoya@gmail.com>
2026-01-21 00:34:05 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  if (event.action === "open") {
+    event.waitUntil(
+      clients.openWindow("/")
+    );
+  }
+});
+// ------------------ Service Worker for PWA ------------------
+const APP_VERSION = "1.0.3";
+const CACHE_NAME = `oneweb-${APP_VERSION}`;
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
